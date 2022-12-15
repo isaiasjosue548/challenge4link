9bdf239bb40fad6af45c55c8fc52b920bab9679a
isaiasjosue548 <isaiasq56@gmail.com>
2022-12-15 17:55:00 +0100

    Bug fixed

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -38,6 +38,7 @@ export const MenuPage = () => {
             <button 
             className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900'
             onClick={() => dispatch ( changeNotAlergic () ) }
+            disabled={!isAlergic}
             >Sin Alergenos</button>
 
         </div>
